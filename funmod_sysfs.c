38b0e6b465e80d973df68b0ee9898de18b7ceb64
Daniel Schaefer <git@danielschaefer.me>
2018-05-05 22:59:27 +0700

    Share number between config and sysfs

--- funmod_sysfs.c
+++ funmod_sysfs.c
@@ -12,23 +12,22 @@ module_param(number, int, 0644);
 MODULE_PARM_DESC(number, "Some number");
 
 static struct kobject *foo_kobject;
-static int value;
 
 static ssize_t foo_read(struct kobject *kobj, struct kobj_attribute *attr,
 			 char *buf)
 {
-	return sprintf(buf, "%d\n", value);
+	return sprintf(buf, "%d\n", number);
 }
 
 static ssize_t foo_write(struct kobject *kobj, struct kobj_attribute *attr,
 			 const char *buf, size_t count)
 {
-	sscanf(buf, "%du", &value);
+	sscanf(buf, "%du", &number);
 	return count;
 }
 
 static struct kobj_attribute fun_attribute =
-	__ATTR(value, 0660, foo_read, foo_write);
+	__ATTR(number, 0660, foo_read, foo_write);
 
 int sysfs_init(void)
 {
@@ -38,12 +37,12 @@ int sysfs_init(void)
 		return -ENOMEM;
 
 	if ((err = sysfs_create_file(foo_kobject, &fun_attribute.attr)))
-		pr_debug("failed to crate the file at /sys/funfile/value");
+		pr_debug("failed to crate the file at /sys/funfile/number");
 	return err;
 }
 
 void sysfs_exit(void)
 {
 	kobject_put(foo_kobject);
-	printk(KERN_ALERT "Today's number was %d", value);
+	printk(KERN_ALERT "Today's number was %d", number);
 }
