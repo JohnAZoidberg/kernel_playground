4c9ba1a1007af124d4c3551e835365c10e0392e4
Daniel Schaefer <git@danielschaefer.me>
2018-05-06 22:07:22 +0700

    Add support for 3 ramdisks

--- ramdisk.c
+++ ramdisk.c
@@ -39,16 +39,15 @@ static unsigned long sector_count = 204800;
 module_param(sector_count, ulong, S_IRUGO);
 MODULE_PARM_DESC(sector_count, "Count of sectors the ramdisk should contain. One sector is 512 bytes.");
 
-
 #define SECTOR_SIZE 512
+#define DEV_COUNT 3
 
+static DEFINE_MUTEX(ramdisk_devices_mutex);
 
 static int major = 0;
-#define DEV_COUNT 1
 static struct gendisk *disks[DEV_COUNT];
-static struct request_queue *queue = NULL;
 static DEFINE_SPINLOCK(spinlock);
-static char *memory = NULL;
+static char *memory[DEV_COUNT];
 static struct block_device_operations bdops = {
 	.owner = THIS_MODULE,
 };
@@ -66,6 +65,8 @@ static void ramdisk_request(struct request_queue *q)
 	req = blk_fetch_request(q);
 
 	while (req) {
+		//printk(KERN_INFO "Request for minor: %d", req->rq_disk->first_minor);
+		//printk(KERN_INFO "Request for minor: %s", req->rq_disk->disk_name);
 		// Iterate through each page of the BIO structure
 		rq_for_each_segment(bvec, req, iter) {
 			// BIO gives you a whole page in which there is data that should be written from or to.
@@ -81,7 +82,7 @@ static void ramdisk_request(struct request_queue *q)
 				continue;
 			}
 			//Block devices access sectors, we have to calculate the memory address out of it.
-			memoryAddr = memory + (SECTOR_SIZE * iter.bio->bi_iter.bi_sector);
+			memoryAddr = memory[req->rq_disk->first_minor] + (SECTOR_SIZE * iter.bio->bi_iter.bi_sector); // TODO FIXME!!!
 
 			if (bio_data_dir(iter.bio) == READ)
 				memcpy(bioAddr, memoryAddr, bvec.bv_len);
@@ -96,58 +97,80 @@ static void ramdisk_request(struct request_queue *q)
 	}
 }
 
+static struct kobject *ramdisk_probe(dev_t dev, int *part, void *data)
+{
+	printk(KERN_INFO "Part is: %d.", *part);
+	return get_disk_and_module(disks[0]);
+}
+
 
 // Initialization
 static int __init ramdisk_init(void)
 {
-	int disk;
-	// Allocate the disk space
-	memory = vmalloc(sector_count * SECTOR_SIZE);
-	if (!memory) {
-		printk(KERN_ERR "Failed to acquire a big chunk of memory for the disk space!");
-		return -ENOMEM;
-	}
+	int disk, err;
 
 	// Register the block device
 	major = register_blkdev(0, "ramdisk");
-	if (major < 1) {
+	if (major < 1) { // TODO check what values the return value can have
 		printk(KERN_ERR "Failed to acquire device file!");
-		vfree(memory);
-		return -EIO;
+		return major;
 	}
+	printk(KERN_INFO "Registered blkdev");
 
-	// Generate a request_queue, it is needed for getting access requests from the kernel
-	queue = blk_init_queue(&ramdisk_request, &spinlock);
-	if (!queue) {
-		printk(KERN_ERR "Failed to generate request_queue struct!");
-		unregister_blkdev(major, "ramdisk");
-		vfree(memory);
-		return -ENOMEM;
-	}
+	blk_register_region(MKDEV(major, 0), 8, THIS_MODULE, ramdisk_probe, NULL, NULL);
+	printk(KERN_INFO "Registered registered region");
 
 	for (disk = 0; disk < DEV_COUNT; disk++) {
+		printk(KERN_INFO "Trying to create ramdisk%d.", disk);
+		// Allocate the disk space
+		memory[disk] = vmalloc(sector_count * SECTOR_SIZE);
+		if (!memory[disk]) {
+			printk(KERN_ERR "Failed to acquire a big chunk of memory for the disk space!");
+			err = -ENOMEM;
+			goto out_free_cleanup_queue;
+		}
+		printk(KERN_INFO "Allocated memory for disk");
 		// Allocate a gendisk struct and initialize with the values of our ramdisk
-		disks[disk] = alloc_disk(16);
+		disks[disk] = alloc_disk(1); // TODO do we need 16? 1 should be enough. What does it mean?
 		if (!disks[disk]) {
 			printk(KERN_ERR "Failed to generate gendisk struct!");
-			blk_cleanup_queue(queue);
-			unregister_blkdev(major, "ramdisk");
-			vfree(memory);
-			return -ENOMEM;
+			err = -ENOMEM;
+			goto out_free_cleanup_queue;
 		}
+		printk(KERN_INFO "Allocated disk");
 
 		disks[disk]->major = major;
-		disks[disk]->first_minor = 0;
+		disks[disk]->first_minor = disk;
 		set_capacity(disks[disk], sector_count);
-		sprintf(disks[disk]->disk_name, "ramdisk");
+		sprintf(disks[disk]->disk_name, "ramdisk%d", disk);
 		disks[disk]->fops = &bdops;
-		disks[disk]->queue = queue;
+		// Generate a request_queue, it is needed for getting access requests from the kernel
+		disks[disk]->queue = blk_init_queue(&ramdisk_request, &spinlock);
+		if (!disks[disk]->queue) {
+			printk(KERN_ERR "Failed to generate request_queue struct!");
+			err = -ENOMEM;
+			goto out_free_cleanup_queue;
+		}
 
 		// HELL YEAH, IT WORKED!
-		printk(KERN_INFO "Successfully created ramdisk.");
 		add_disk(disks[disk]);
+		printk(KERN_INFO "Successfully created/added ramdisk%d.", disk);
 	}
+	printk(KERN_INFO "Successfully created all ramdisks.");
 	return 0;
+out_free_cleanup_queue:
+	printk(KERN_INFO "Ramdisk: out_free_cleanup_queue (vfree)");
+	for (disk = 0; disk < DEV_COUNT; disk++) {
+		if (memory[disk])
+			vfree(memory[disk]);
+		if (disks[disk]->queue)
+			blk_cleanup_queue(disks[disk]->queue);
+	}
+	printk(KERN_INFO "Ramdisk: out_free_cleanup_queue (blk_cleanup_queue)");
+	printk(KERN_INFO "Ramdisk: out_unregister");
+	blk_unregister_region(MKDEV(major, 0), 1UL << MINORBITS);
+	unregister_blkdev(major, "ramdisk");
+	return err;
 }
 
 
@@ -160,10 +183,15 @@ static void __exit ramdisk_exit(void)
 	for (disk = 0; disk < DEV_COUNT; disk++) {
 		del_gendisk(disks[disk]);
 		put_disk(disks[disk]);
+		if (memory[disk])
+			vfree(memory[disk]);
+		if (disks[disk]->queue)
+			blk_cleanup_queue(disks[disk]->queue);
+		printk(KERN_INFO "Successfully destroyed ramdisk%d.", disk);
 	}
-	blk_cleanup_queue(queue);
+	blk_unregister_region(MKDEV(major, 0), 1UL << MINORBITS);
 	unregister_blkdev(major, "ramdisk");
-	vfree(memory);
+	printk(KERN_INFO "Every ramdisk successfully destroyed...");
 }
 
 
